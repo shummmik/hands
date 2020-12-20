48a4abcf2e156df7f05925a78eeff73d1769d088
wuser <Developproddp4>
2020-12-20 13:52:49 +0300

    fix error

--- prepare.sh
+++ prepare.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # See https://sdkman.io/install
-apt-get update
-apt-get -q -y install curl zip unzip git
-curl -s https://get.sdkman.io | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-# See https://www.scala-sbt.org/1.x/docs/Installing-sbt-on-Linux.html
-
-
-sdk install java 15.0.1.j9-adpt
-sdk install sbt
-
-sbt clean compile test
-
-
-#apt -q -y install gnupg, curl
+#apt-get update
+#apt-get -q -y install curl zip unzip git
+#curl -s https://get.sdkman.io | bash
+#source "$HOME/.sdkman/bin/sdkman-init.sh"
 #
-#echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
-#curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
-#apt update
-#apt install -y sbt
+## See https://www.scala-sbt.org/1.x/docs/Installing-sbt-on-Linux.html
 #
-#apt install -y openjdk-11-jdk  #8 23
+#
+#sdk install java 11.0.9.hs-adpt
+#sdk install sbt
+#
+#sbt clean compile test
+
+
+apt -q -y install gnupg, curl
+
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
+apt update
+apt install -q -y sbt
+
+apt install -q -y openjdk-11-jdk  #8 32
